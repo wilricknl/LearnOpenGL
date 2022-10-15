a886339050457f3468adc575d3b7441eedd4be13
wilricknl <83393813+wilricknl@users.noreply.github.com>
2022-10-15 15:25:49 +0200

    Create lighting project

---
+++ lighting/main.cpp
@@ -0,0 +1,228 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+#include "common/camera.hpp"
+#include "common/shader.hpp"
+#include "config.hpp"
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void scrollCallback(GLFWwindow* window, double offsetX, double offsetY);
+void mouseCallback(GLFWwindow* window, double positionX, double positionY);
+void processInput(GLFWwindow* window);
+
+// screen
+float horizontal = 800.0f;
+float vertical = 600.0f;
+
+// camera
+bool bFirstMouse = true;
+float lastMouseX = 400.0f;
+float lastMouseY = 300.0f;
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+// timing
+float deltaTime;
+float lastFrame;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    auto* window = glfwCreateWindow(horizontal, vertical, "Hello, OpenGL!", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create the window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouseCallback);
+    glfwSetScrollCallback(window, scrollCallback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    Shader cubeShader("shader.vert", "shader.frag");
+    Shader lightShader("light.vert", "light.frag");
+
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+
+    unsigned int vbo, cubeVao;
+    glGenVertexArrays(1, &cubeVao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(cubeVao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    unsigned int lightVao;
+    glGenVertexArrays(1, &lightVao);
+    glBindVertexArray(lightVao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        const auto currentTime = static_cast<float>(glfwGetTime());
+        deltaTime = currentTime - lastFrame;
+        lastFrame = currentTime;
+
+        processInput(window);
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        cubeShader.Use();
+        cubeShader.SetVec3("objectColor", 1.0f, 0.5f, 0.31f);
+        cubeShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
+
+        const auto projection = glm::perspective(glm::radians(camera.GetFov()), horizontal / vertical, 0.1f, 100.0f);
+        const auto view = camera.GetViewMatrix();
+        auto model = glm::mat4(1.0f);
+        cubeShader.SetMat4("projection", projection);
+        cubeShader.SetMat4("view", view);
+        cubeShader.SetMat4("model", model);
+
+        glBindVertexArray(cubeVao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glm::vec3 lightPosition(-1.2f, 1.0f, -2.0f);
+
+        lightShader.Use();
+        lightShader.SetMat4("projection", projection);
+        lightShader.SetMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, lightPosition);
+        model = glm::scale(model, glm::vec3(0.2f));
+        lightShader.SetMat4("model", model);
+
+        glBindVertexArray(cubeVao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &cubeVao);
+    glDeleteVertexArrays(1, &lightVao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteProgram(cubeShader.GetId());
+    glDeleteProgram(lightShader.GetId());
+
+    glfwTerminate();
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void scrollCallback(GLFWwindow* window, double offsetX, double offsetY)
+{
+    camera.ProcessMouseScroll(static_cast<float>(offsetY));
+}
+
+void mouseCallback(GLFWwindow* window, double positionX, double positionY)
+{
+    if (bFirstMouse)
+    {
+        lastMouseX = static_cast<float>(positionX);
+        lastMouseY = static_cast<float>(positionY);
+        bFirstMouse = false;
+    }
+
+    float offsetX = static_cast<float>(positionX) - lastMouseX;
+    float offsetY = lastMouseY - static_cast<float>(positionY);
+    lastMouseX = static_cast<float>(positionX);
+    lastMouseY = static_cast<float>(positionY);
+
+    camera.ProcessMouseMovement(offsetX, offsetY);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(CameraMovement::Forward, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(CameraMovement::Backward, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(CameraMovement::Right, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        camera.ProcessKeyboard(CameraMovement::Left, deltaTime);
+    }
+}
