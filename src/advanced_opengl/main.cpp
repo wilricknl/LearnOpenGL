c5c1aa2e25f2dfd115febff2866404b67dda9876
wilricknl <83393813+wilricknl@users.noreply.github.com>
2022-12-23 15:12:26 +0100

    Add custom framebuffer
    #LOGL-13 In Progress

--- src/advanced_opengl/main.cpp
+++ src/advanced_opengl/main.cpp
@@ -91,7 +91,71 @@ int main()
         glm::vec3(5.5f, 1.0f, -0.6f)
     };
 
-    while (!glfwWindowShouldClose(window))
+
+	// framebuffer
+    unsigned int framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+
+    // framebuffer texture
+    unsigned int texColorBuffer;
+    glGenTextures(1, &texColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, (int)horizontal, (int)vertical, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // attach texture to framebuffer
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+
+    // render buffer
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, (int)horizontal, (int)vertical);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    // attach render buffer to framebuffer
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    // check framebuffer correctness
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        std::cout << "ERROR::FRAMEBUFFER::INCOMPLETE" << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // quad to render to
+    Shader quadShader{ "quad.vert", "quad.frag" };
+
+    float quadVertices[]
+    {
+        -1.0f,  1.0f, 0.0f, 1.0f,
+        -1.0f, -1.0f, 0.0f, 0.0f,
+         1.0f, -1.0f, 1.0f, 0.0f,
+
+        -1.0f,  1.0f, 0.0f, 1.0f,
+         1.0f, -1.0f, 1.0f, 0.0f,
+         1.0f,  1.0f, 1.0f, 1.0f,
+    };
+
+    unsigned int quadVao, quadVbo;
+    glGenVertexArrays(1, &quadVao);
+    glGenBuffers(1, &quadVbo);
+    glBindVertexArray(quadVao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, quadVbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, nullptr);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)(2 * sizeof(float)));
+    // framebuffer end
+
+
+	while (!glfwWindowShouldClose(window))
     {
         const auto currentTime = static_cast<float>(glfwGetTime());
         deltaTime = currentTime - lastFrame;
@@ -99,8 +163,12 @@ int main()
 
         processInput(window);
 
+
+    	// first pass with custom frame buffer
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
         glClearColor(0.45f, 0.45f, 0.45f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+        glEnable(GL_DEPTH_TEST);
 
         const auto projection = glm::perspective(glm::radians(camera.GetFov()), horizontal / vertical, 0.1f, 100.0f);
         const auto view = camera.GetViewMatrix();
@@ -130,6 +198,19 @@ int main()
             grass.Draw(shader, model, view, projection);
         }
         glEnable(GL_CULL_FACE);
+
+
+    	// second pass with default framebuffer
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        quadShader.Use();
+        glBindVertexArray(quadVao);
+        glDisable(GL_DEPTH_TEST);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -175,6 +256,19 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	{
         bOutline = !bOutline;
 	}
+    static bool bWireframe = true;
+    if (key == GLFW_KEY_TAB && action == GLFW_PRESS)
+    {
+        if (bWireframe)
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        }
+        else
+        {
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        }
+        bWireframe = !bWireframe;
+    }
 }
 
 void processInput(GLFWwindow* window)
